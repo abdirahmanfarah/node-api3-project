44d51e39ab0339577d9e09c36294726d7b0070bf
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2020-01-16 12:04:31 -0800

    changed 4000 to port on index

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 const server = require('./server.js');
 
 const port = process.env.PORT || 4000;
-server.listen(4000, () => {
+
+server.listen(port, () => {
   console.log(`\n* Server Running on http://localhost:${port} *\n`)
 });
 
